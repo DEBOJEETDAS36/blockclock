1377ace7317fb566361233ff51d532dd4dc8599d
Debojeet <119153713+DEBOJEETDAS36@users.noreply.github.com>
2025-05-31 01:41:39 +0530

    Update recordtime.sol

--- recordtime.sol
+++ recordtime.sol
@@ -1,1 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract DeploymentTime {
+    uint256 public timestamp;
+
+    constructor() {
+        timestamp = block.timestamp;
+    }
+}
 
